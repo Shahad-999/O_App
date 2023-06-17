e91ec39d749ef5a3c289e296bce22b046b4a86e5
Shahad-999 <shahad.h.999.a@gmail.com>
2023-06-17 13:10:05 +0300

    add animation to start button at home screen

--- app/src/main/java/com/shahad/o/ui/util/ComposeExtensions.kt
+++ app/src/main/java/com/shahad/o/ui/util/ComposeExtensions.kt
@@ -1,9 +1,11 @@
 package com.shahad.o.ui.util
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.navigation.NavHostController
 import com.shahad.o.ui.navigation.Screens
 
@@ -26,5 +28,25 @@ fun DrawScope.circle(
         useCenter = false,
         style = Stroke(width = strokeWidth, cap = StrokeCap.Round),
     )
+
+}
+
+fun DrawScope.gradiantCircle(
+    colors: List<Color>,
+    rotateAnimation: Float,
+    strokeWidth: Float
+) {
+    rotate(
+        rotateAnimation
+    ) {
+
+        drawArc(
+            brush = Brush.sweepGradient(colors),
+            startAngle = 0f,
+            sweepAngle = 360f,
+            useCenter = false,
+            style = Stroke(width = strokeWidth, cap = StrokeCap.Round)
+        )
+    }
 
 }
--- app/src/main/java/com/shahad/o/ui/views/widgets/Buttons.kt
+++ app/src/main/java/com/shahad/o/ui/views/widgets/Buttons.kt
@@ -1,22 +1,39 @@
 package com.shahad.o.ui.views.widgets
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.shahad.o.R
 import com.shahad.o.ui.theme.OTheme
+import com.shahad.o.ui.util.gradiantCircle
 
 @Composable
 fun OButton(
@@ -79,6 +96,55 @@ fun OutlineOButton(
     }
 }
 
+@Composable
+fun StartButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+){
+    Box(
+        modifier = modifier
+            .padding(top = 32.dp)
+            .size(125.dp),
+        contentAlignment = Alignment.Center
+    ) {
+
+        val colors = OTheme.colors.gradiantColors
+        val infiniteTransition = rememberInfiniteTransition()
+        val rotateAnimation by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = 1000,
+                    easing = LinearEasing
+                )
+            )
+        )
+
+        OCircle(
+            modifier = Modifier
+                .clip(CircleShape)
+                .clickable { onClick() },
+            size = 125.dp,
+        ) {
+            gradiantCircle(
+                colors = colors,
+                rotateAnimation = rotateAnimation,
+                strokeWidth = 30F
+            )
+        }
+        Text(
+            text = stringResource(R.string.start),
+            style = TextStyle(
+                color = OTheme.colors.primary,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
 
 @Preview
 @Composable
@@ -104,6 +170,19 @@ fun OutlineOButtonPreview() {
                 text = "Login With Google"
             ) {
 
+            }
+        }
+    }
+}
+
+
+
+@Preview
+@Composable
+fun StartButtonPreview() {
+    OTheme {
+        Surface {
+            StartButton{
             }
         }
     }
